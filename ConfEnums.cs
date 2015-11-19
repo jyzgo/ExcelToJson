9334b5330f6184a4e745204eaed417e62cebcfa2
jyzgo <jiangyizheng@magictavern.com>
2015-11-19 14:40:23 +0800

    add excel and the json that output

---
+++ ConfEnums.cs
@@ -0,0 +1,128 @@
+
+public enum CommonConfEnum{
+	energyMax,
+	recoverInterval,
+	costEnergy,
+	isCostWin,
+	initCash,
+	hintInterval,
+	timeChange,
+}
+
+public enum ElimConfEnum{
+	h3,
+	h4,
+	h5,
+	h6,
+	v3,
+	v4,
+	v5,
+	v6,
+	L33,
+	L34,
+	T33,
+	C33,
+	S22,
+	D51,
+}
+
+public enum BombWeightEnum{
+	verticalBomb,
+	horizontalBomb,
+	squareBomb,
+	colorBomb,
+	fishBomb,
+	dyeBomb,
+	crossBomb,
+	rndStripBomb,
+}
+
+public enum BombGenEnum{
+	h3,
+	h4,
+	h5,
+	h6,
+	v3,
+	v4,
+	v5,
+	v6,
+	L33,
+	L34,
+	T33,
+	C33,
+	S22,
+	D51,
+}
+
+public enum PropConfEnum{
+	spoon,
+	cross,
+	color,
+}
+
+public enum StartPropConfEnum{
+	startStrip,
+	startSquare,
+	startColor,
+}
+
+public enum MechanismEnum{
+	jim1,
+	jim2,
+	jim3,
+	jim4,
+	jim5,
+	jim6,
+	jim7,
+	jim8,
+	jim9,
+	jim10,
+	jim11,
+	jim12,
+	jim13,
+	jim14,
+}
+
+public enum TurnConfEnum{
+	jim1,
+	jim2,
+	jim3,
+	jim4,
+	jim5,
+	jim6,
+	jim7,
+	jim8,
+	jim9,
+	jim10,
+	jim11,
+	jim12,
+	jim13,
+	jim14,
+}
+
+public enum PropPayEnum{
+	startStrip,
+	startSquare,
+	startColor,
+	spoon,
+	cross,
+	color,
+}
+
+public enum DiscountConfEnum{
+	jim1,
+	jim2,
+	jim3,
+	jim4,
+	jim5,
+	jim6,
+	jim7,
+	jim8,
+	jim9,
+	jim10,
+	jim11,
+	jim12,
+	jim13,
+	jim14,
+}
+
